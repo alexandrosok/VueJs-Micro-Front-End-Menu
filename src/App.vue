b476613a2d2768f2b8ee5d026040948f533f4ca1
alexandros <alexandrosok@hotmail.com>
2019-09-24 16:00:31 +0200

    initializing

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="menu">
+    <Navigation/>
+  </div>
+</template>
+<script>
+
+  import Navigation from './components/Navigation.vue'
+
+  export default {
+    name: 'MenuApp',
+    components: {
+      Navigation
+    }
+  }
+
+</script>
+
+<style>
+
+  #menu {
+    width: 30%;
+  }
+
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="side-navigation">
+    <ul>
+      <li @click="redirect(item.title)" class="menu-item" v-for="(item , index) in items" :key="index">
+        {{item.title}}
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Navigation',
+    data () {
+      return {
+        items: [
+          {title: 'Page 1', redirect: ''},
+          {title: 'Page 2', redirect: ''},
+          {title: 'Page 3', redirect: ''},
+        ],
+        right: null,
+        msg: 'Navigation'
+      }
+    },
+    beforeMount () {
+    },
+    methods: {
+      redirect (page) {
+        var evt = new CustomEvent('redirect', {detail: page})
+        window.dispatchEvent(evt)
+      }
+    },
+  }
+</script>
+<style scoped>
+
+  .menu-item:hover {
+    background: green;
+  }
+
+  .menu-item {
+    list-style: none;
+    border: 1px solid black;
+    padding: 5px;
+    height: 30px;
+  }
+
+</style>
